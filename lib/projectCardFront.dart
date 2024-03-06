b10134058ce58c0e0ae917650fcd5d458ce81b42
Angelika Ostrowska <01178572@pw.edu.pl>
2024-03-06 13:37:00 +0100

    Fixed scrollbars being visible on cards

--- lib/projectCardFront.dart
+++ lib/projectCardFront.dart
@@ -6,17 +6,20 @@ class ProjectCardFront extends Card {
   ProjectCardFront(this.context, this.name, this.description, this.imgPath,
       {super.key})
       : super(
-            child: SingleChildScrollView(
-          physics: const NeverScrollableScrollPhysics(),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text(name, style: Theme.of(context).textTheme.bodyLarge),
-                Text(
-                  description,
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Image.asset(imgPath),
-              ]),
+            child: ScrollConfiguration(
+          behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+          child: SingleChildScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(name, style: Theme.of(context).textTheme.bodyLarge),
+                  Text(
+                    description,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  Image.asset(imgPath),
+                ]),
+          ),
         ));
 }
