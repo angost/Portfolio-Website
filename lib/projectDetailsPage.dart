ccde2f5738dada189c3b966b5d9fc248d5f769ff
Angelika Ostrowska <01178572@pw.edu.pl>
2024-03-11 17:13:03 +0100

    Created basic ProjectDetailsPage layout

--- lib/projectDetailsPage.dart
+++ lib/projectDetailsPage.dart
@@ -18,8 +18,43 @@ class _ProjectDetailsPageState extends State<ProjectDetailsPage> {
     return Scaffold(
       appBar: MyAppBar(context, "Projects", true), // add back navigation arrow
       body: Center(
-        child: Text(widget.projectDetails['name']),
-      ),
+          child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white,
+                  border: Border.all(
+                      width: 3, color: Theme.of(context).primaryColor)),
+              margin: const EdgeInsets.only(
+                  left: 60.0, right: 60.0, top: 40.0, bottom: 40.0),
+              child: Column(children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.red,
+                    // child: Text(widget.projectDetails['name']),
+                  ),
+                ),
+                Expanded(
+                  flex: 9,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(flex: 1, child: Container(color: Colors.amber)),
+                      Expanded(flex: 3, child: Container(color: Colors.green))
+                    ],
+                  ),
+                )
+              ]),
+            ),
+          ),
+          Container(
+            color: Color.fromRGBO(217, 217, 217, 1),
+            height: 50,
+          )
+        ],
+      )),
     );
   }
 }
