49da4c238b2052a6b3646759cd4a25669a715b77
Angelika Ostrowska <01178572@pw.edu.pl>
2024-03-18 23:34:39 +0100

    Added contact buttons

--- lib/contactPage.dart
+++ lib/contactPage.dart
@@ -2,8 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:portfolio_app/myAppBar.dart';
+import 'package:portfolio_app/contactButton.dart';
 
 class ContactPage extends StatefulWidget {
   const ContactPage({super.key});
@@ -18,57 +18,25 @@ class _ContactPageState extends State<ContactPage> {
     double windowWidth = MediaQuery.of(context).size.width;
     double windowHeight = MediaQuery.of(context).size.height;
 
+    Uri githubLink = Uri(scheme: "", host: "", path: "github.com/angost");
+    Uri linkedinLink =
+        Uri(scheme: "", host: "", path: "linkedin.com/in/angelikaostrowska");
+    Uri cvLink = Uri(scheme: "", host: "", path: "aostrowska.pl/CV.pdf");
+
     return Scaffold(
       appBar: MyAppBar(context, "Contact", false, windowWidth > windowHeight),
       body: Center(
           child: Column(children: <Widget>[
         Spacer(),
         Text('angelikaostrowska@poczta.onet.pl'),
-        // SizedBox(height: 40),
-        // ElevatedButton.icon(
-        //   style: ButtonStyle(
-        //     padding: MaterialStateProperty.all<EdgeInsets>(
-        //         const EdgeInsets.only(
-        //             left: 15, right: 15, top: 10, bottom: 10)),
-        //     overlayColor: MaterialStateProperty.all<Color>(
-        //         const Color.fromRGBO(201, 116, 149, 0.5)),
-        //     backgroundColor: MaterialStateProperty.all<Color>(
-        //         const Color.fromRGBO(227, 213, 219, 0.5)),
-        //     shadowColor: MaterialStateProperty.all<Color>(Colors.transparent),
-        //     foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-        //     splashFactory: NoSplash.splashFactory,
-        //   ),
-        //   onPressed: () => launch('www.linkedin.com/in/angelikaostrowska'),
-        //   label: Text('Linkedin'),
-        //   icon: Icon(Icons.person),
-        // ),
-        // SizedBox(height: 40),
-        // ElevatedButton.icon(
-        //   style: ButtonStyle(
-        //     padding: MaterialStateProperty.all<EdgeInsets>(
-        //         const EdgeInsets.only(
-        //             left: 15, right: 15, top: 10, bottom: 10)),
-        //     overlayColor: MaterialStateProperty.all<Color>(
-        //         const Color.fromRGBO(201, 116, 149, 0.5)),
-        //     backgroundColor: MaterialStateProperty.all<Color>(
-        //         const Color.fromRGBO(227, 213, 219, 0.5)),
-        //     shadowColor: MaterialStateProperty.all<Color>(Colors.transparent),
-        //     foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-        //     splashFactory: NoSplash.splashFactory,
-        //   ),
-        //   onPressed: () async => _launchUrl('CV.pdf'),
-        //   label: Text('CV'),
-        //   icon: Icon(Icons.edit_document),
-        // ),
+        SizedBox(height: 40),
+        ContactButton("LinkedIn", Icon(Icons.person), linkedinLink),
+        SizedBox(height: 40),
+        ContactButton("Github", Icon(Icons.code), githubLink),
+        SizedBox(height: 40),
+        ContactButton("CV", Icon(Icons.edit_document), cvLink),
         Spacer(),
       ])),
     );
   }
-
-  // Future<void> _launchUrl(String link) async {
-  //   Uri _url = Uri.parse(link);
-  //   if (!await launchUrl(_url)) {
-  //     throw Exception('Could not launch $_url');
-  //   }
-  // }
 }
