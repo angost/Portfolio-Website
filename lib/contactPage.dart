cccf4c870ec8781980dab78c630e97dd887bf72b
Angelika Ostrowska <01178572@pw.edu.pl>
2024-03-22 12:06:45 +0100

    Added CV in Polish, made email selectable text

--- lib/contactPage.dart
+++ lib/contactPage.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+import 'package:portfolio_app/theme/theme_constants.dart';
 import 'package:portfolio_app/myAppBar.dart';
 import 'package:portfolio_app/contactButton.dart';
 
@@ -21,20 +22,33 @@ class _ContactPageState extends State<ContactPage> {
     Uri githubLink = Uri(scheme: "", host: "", path: "github.com/angost");
     Uri linkedinLink =
         Uri(scheme: "", host: "", path: "linkedin.com/in/angelikaostrowska");
-    Uri cvLink = Uri(scheme: "", host: "", path: "aostrowska.pl/CV.pdf");
+    Uri cvEnglishLink = Uri(scheme: "", host: "", path: "aostrowska.pl/CV.pdf");
+    Uri cvPolishLink =
+        Uri(scheme: "", host: "", path: "aostrowska.pl/CV-PL.pdf");
 
     return Scaffold(
       appBar: MyAppBar(context, "Contact", false, windowWidth > windowHeight),
       body: Center(
           child: Column(children: <Widget>[
         Spacer(),
-        Text('angelikaostrowska@poczta.onet.pl'),
+        SelectableText(
+          'angelikaostrowska@poczta.onet.pl',
+          style: textBodyMedium,
+        ),
+        SizedBox(height: 40),
+        Row(children: <Widget>[
+          Spacer(),
+          ContactButton("CV English", Icon(Icons.edit_document), cvEnglishLink),
+          SizedBox(
+            width: 40,
+          ),
+          ContactButton("CV Polish", Icon(Icons.edit_document), cvPolishLink),
+          Spacer(),
+        ]),
         SizedBox(height: 40),
         ContactButton("LinkedIn", Icon(Icons.person), linkedinLink),
         SizedBox(height: 40),
         ContactButton("Github", Icon(Icons.code), githubLink),
-        SizedBox(height: 40),
-        ContactButton("CV", Icon(Icons.edit_document), cvLink),
         Spacer(),
       ])),
     );
