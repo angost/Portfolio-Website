517ee665a9d3c1e639af2bcc29ec5f2fefc7d45f
Angelika Ostrowska <01178572@pw.edu.pl>
2024-06-27 21:59:17 +0200

    Changed method of fixing card's back text

--- lib/projectCardBack.dart
+++ lib/projectCardBack.dart
@@ -17,40 +17,44 @@ class ProjectCardBack extends Card {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Expanded(
+                    flex: 2,
                     child: Padding(
-                  padding: const EdgeInsets.only(left: 10, right: 10),
-                  child: Center(
-                    child: AutoSizeText(
-                      description,
-                      style: textBodySmall,
-                    ),
-                  ),
-                )),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        PageRouteBuilder(
-                          pageBuilder: (context, animation1, animation2) =>
-                              ProjectDetailsPage(projectDetails),
-                          transitionDuration: Duration.zero,
-                          reverseTransitionDuration: Duration.zero,
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: Center(
+                        child: AutoSizeText(
+                          description,
+                          style: textBodySmall,
                         ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.only(
-                            top: 15, bottom: 15, left: 20, right: 20),
-                        backgroundColor: const Color.fromRGBO(227, 203, 214, 1),
-                        foregroundColor:
-                            const Color.fromRGBO(201, 116, 149, 0.5),
-                        textStyle: textButtons,
-                        shadowColor: Colors.transparent),
-                    child: Text(
-                      "See more",
-                      style: textButtons.copyWith(color: Colors.black),
+                      ),
+                    )),
+                Expanded(
+                  flex: 1,
+                  child: UnconstrainedBox(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          PageRouteBuilder(
+                            pageBuilder: (context, animation1, animation2) =>
+                                ProjectDetailsPage(projectDetails),
+                            transitionDuration: Duration.zero,
+                            reverseTransitionDuration: Duration.zero,
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.only(
+                              top: 15, bottom: 15, left: 20, right: 20),
+                          backgroundColor:
+                              const Color.fromRGBO(227, 203, 214, 1),
+                          foregroundColor:
+                              const Color.fromRGBO(201, 116, 149, 0.5),
+                          textStyle: textButtons,
+                          shadowColor: Colors.transparent),
+                      child: Text(
+                        "See more",
+                        style: textButtons.copyWith(color: Colors.black),
+                      ),
                     ),
                   ),
                 ),
